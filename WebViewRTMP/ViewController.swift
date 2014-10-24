83bbe9421bee2e11488113c41e4fe423952cbe64
thanawat.s <thehuks@gmail.com>
2014-10-24 20:08:00 +0700

    First commit

--- WebViewRTMP/ViewController.swift
+++ WebViewRTMP/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import MediaPlayer
+
 
 class ViewController: UIViewController {
+    
+    //@IBOutlet weak var _video: UIWebView!
+    @IBOutlet weak var _view: UIView!
+    
+    var moviePlayer:MPMoviePlayerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let strURL = "http://122.155.180.125/live/_definst_/workpoint_480p/playlist.m3u8"
+        let url = NSURL(string: strURL)
+        //let urlRequest = NSURLRequest(URL: url)
+        
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        moviePlayer.view.frame = CGRectMake(0, 0, 340, 250)
+        
+        self._view.addSubview(moviePlayer.view)
+        
+        moviePlayer.fullscreen = true
+        
+        moviePlayer.controlStyle = MPMovieControlStyle.Embedded
+        
+        //_video.loadRequest(urlRequest)
+        
     }
 
     override func didReceiveMemoryWarning() {
